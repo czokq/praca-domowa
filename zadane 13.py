1b96b9b7c1a87e9d7f71327bf9100b3fb72d6738
czokq <marek.sarbiewski@gmail.com>
2023-10-10 18:07:25 +0200

    wwwww

---
+++ zadane 13.py
@@ -0,0 +1,14 @@
+import sys
+ocena = int(input("podaj swoją ocenę z klasy 8: "))
+wynik = int(input("podaj swój wynik z  testu: "))
+
+if wynik >= 90:
+    print("dostałeś się do grupy zawansowanej")
+    sys.exit(0)
+
+if ocena >= 5:
+    print("dostałeś się do grupy zawansowanej")
+    sys.exit(0)
+else:
+    print("dostałeś się do grupy niezawannsowanej")
+    sys.exit(0)
